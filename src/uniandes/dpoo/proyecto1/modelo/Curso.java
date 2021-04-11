a611b6fc3ee6ef9338ba30928bc38275fa2566ef
Phelyxx <habbowalkvps@gmail.com>
2021-04-10 22:37:48 -0500

    1

--- src/uniandes/dpoo/proyecto1/modelo/Curso.java
+++ src/uniandes/dpoo/proyecto1/modelo/Curso.java
@@ -1,21 +1,38 @@
 package uniandes.dpoo.proyecto1.modelo;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class Curso 
 {
-   private String nombre;
-   private int creditos;
-   private boolean esObligatoria;
-   private boolean electivaIngenieria;
-   private boolean electivaProfesonal;
-   private boolean esCBU;
-   private boolean esTipoE;
-   private boolean EstipoI;
-   private boolean esTipoEpsilon;
-   private ArrayList preRequisitos; 
-   private ArrayList coRequisitos;
-   private int semestre;
+	private String nombre;
+	private int creditos;
+	private boolean esObligatorio;
+	private boolean esElectivaIngenieria;
+	private boolean esElectivaProfesional;
+	private String CBU;
+	private boolean esTipoI;
+	private boolean esTipoE;
+	private boolean esEpsilon;
+	private List<Curso> prerrequisitos;
+	private List<Curso> correquisitos;
+	private int semestre;
    
-   
+	public Curso(String nombre, int creditos, boolean esObligatorio, boolean esElectivaIngenieria, boolean esElectivaProfesional,
+			String CBU, boolean esTipoI, boolean esTipoE, boolean esEpsilon, List<Curso> prerrequisitos, List<Curso> correquisitos, int semestre)
+	{
+		this.nombre = nombre;
+		this.creditos = creditos;
+		this.esObligatorio = esObligatorio;
+		this.esElectivaIngenieria = esElectivaIngenieria;
+		this.esElectivaProfesional = esElectivaProfesional;
+		this.CBU = CBU;
+		this.esTipoI = esTipoI;
+		this.esTipoE = esTipoE;
+		this.esEpsilon = esEpsilon;
+		this.prerrequisitos = new ArrayList<>();
+		this.correquisitos = new ArrayList<>();
+		this.semestre = semestre;
+	}
 }
--- src/uniandes/dpoo/proyecto1/modelo/Estudiante.java
+++ src/uniandes/dpoo/proyecto1/modelo/Estudiante.java
@@ -1,6 +1,9 @@
 package uniandes.dpoo.proyecto1.modelo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
 
 public class Estudiante {
 	
