a26a4f0aba9611eaceab431eafe54080fd3e1f38
pipe2910 <69812266+pipe2910@users.noreply.github.com>
2021-04-10 20:38:57 -0500

    ejecucion

--- src/uniandes/dpoo/proyecto1/procesamiento/Loader.java
+++ src/uniandes/dpoo/proyecto1/procesamiento/Loader.java
@@ -1,5 +1,20 @@
 package uniandes.dpoo.proyecto1.procesamiento;
 
-public class Loader {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Loader
+{
+    public static InformacionPensum cargarArchivo(String nombre) throws FileNotFoundException, IOException
+    {
+    	BufferedReader br = new BufferedReader(new FileReader(nombre));
+		String linea = br.readLine();
+		linea = br.readLine();
+		while (linea != null) 
+		{
+			String[] partes = linea.split(",");
+        }
+    }
 }
